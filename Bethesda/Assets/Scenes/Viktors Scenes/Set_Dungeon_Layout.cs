edd601e0050c5a8547fa14aee51dbca16db0a9c3
Viktor <vicke00@live.com>
2018-09-17 15:59:51 +0200

    17/09 viktor

--- Bethesda/Assets/Scenes/Viktors Scenes/Set_Dungeon_Layout.cs
+++ Bethesda/Assets/Scenes/Viktors Scenes/Set_Dungeon_Layout.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Set_Dungeon_Layout : MonoBehaviour {
 
-    public List<List<short>> map;
+    private GameObject[,] map;
+    public GameObject room;
+    public GameObject[] sideRooms;
+    public GameObject[] cornerRooms;
+    public GameObject[] midRooms;
+
+    public ushort width; //based on direction
+    public ushort girth; //based on direction
+    private int dir;
+    [Range (10.0f,100.0f)]
+    public float distanceBetweenRooms;
 
     // Use this for initialization
     void Start () {
-        
+        DrawGrid();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void DrawGrid()
+    {
+        map = new GameObject[width, girth];
+        dir = Random.Range(0, 4);
+
+        for (int i = 0; i < width; i++) 
+        {
+            for (int k = 0; k < girth; k++)
+            { 
+                map[i, k] = Instantiate(room, new Vector3(k * distanceBetweenRooms, 0, i * distanceBetweenRooms), Quaternion.identity) as GameObject;
+            }
+        }
+    }
 }
