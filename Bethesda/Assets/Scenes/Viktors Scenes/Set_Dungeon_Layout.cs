37f43c0c5015e9c85278f6660ce380fbc25e88b7
Viktor <vicke00@live.com>
2018-09-17 13:07:31 +0200

    Assets, Viktors map
    Signed-off-by: Viktor <vicke00@live.com>

---
+++ Bethesda/Assets/Scenes/Viktors Scenes/Set_Dungeon_Layout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Dungeon_Layout : MonoBehaviour {
+
+    public List<List<short>> map;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
