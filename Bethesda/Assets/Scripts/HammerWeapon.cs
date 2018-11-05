9c7439ebdafef87ae61306d89161e6c3338bfa43
Hirad Salmend <hirad.salmend@learnet.se>
2018-11-05 13:53:18 +0100

    pickup object;

--- Bethesda/Assets/Scripts/HammerWeapon.cs
+++ Bethesda/Assets/Scripts/HammerWeapon.cs
@@ -2,42 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Element
+{
+    Fire,
+    Ice,
+    Lightning,
+    Poison
+}
 [RequireComponent(typeof(AudioSource))]
 public class HammerWeapon : MonoBehaviour
 {
 
-	Collider hitbox;
-	AudioSource audioSource;
-	Animator anim;
+    Collider hitbox;
+    AudioSource audioSource;
+    Animator anim;
 
-	void Awake()
-	{
-		hitbox = transform.Find("Head").GetComponent<Collider>();
-		audioSource = GetComponent<AudioSource>();
-		anim = GetComponent<Animator>();
-	}
+    void Awake()
+    {
+        hitbox = transform.Find("Head").GetComponent<Collider>();
+        audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+    }
 
-	void Update()
-	{
-		if (Input.GetButtonDown("Attack") && !anim.GetCurrentAnimatorStateInfo(0).IsName("PlaceholderWeaponAttack"))
-		{
-			print("Attacku!");
-			anim.Play("PlaceholderWeaponAttack");
-		}
-	}
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack") && !anim.GetCurrentAnimatorStateInfo(0).IsName("PlaceholderWeaponAttack"))
+        {
+            print("Attacku!");
+            anim.Play("PlaceholderWeaponAttack");
+        }
+    }
 
-	public void EnableHitbox(int enable)
-	{
-		hitbox.enabled = enable == 0 ? false : true;
-	}
 
-	public void PlaySound(Object audioClip)
-	{
-		audioSource.PlayOneShot((AudioClip)audioClip);
-	}
 
-	public void ScreenShake()
-	{
-		CameraEffects.Get.Shake(0.25f, 0.4f);
-	}
+    public void EnableHitbox(int enable)
+    {
+        hitbox.enabled = enable == 0 ? false : true;
+    }
+
+    public void PlaySound(Object audioClip)
+    {
+        audioSource.PlayOneShot((AudioClip)audioClip);
+    }
+
+    public void ScreenShake()
+    {
+        CameraEffects.Get.Shake(0.25f, 0.4f);
+    }
 }
---
+++ Bethesda/Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            other.transform.Find("Weapon/Head").GetComponent<MeshRenderer>().material = mat; 
+        }
+    }
+}
