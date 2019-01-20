863750400aa181d339f844e7c40f41013585b2ac
Benjamin Larsson <mafortion.benjamin@gmail.com>
2019-01-20 19:23:21 +0100

    New healthbar

---
+++ Bethesda/Assets/Scripts/FrameByFrameSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameByFrameSlider : MonoBehaviour
+{
+	public Sprite[] frames;
+	public Image image;
+
+	public int frameIdx;
+
+	Slider slider;
+
+	// Use this for initialization
+	void Start()
+	{
+		slider = GetComponent<Slider>();
+		slider.onValueChanged.AddListener(OnValueChanged);
+	}
+	
+	void OnValueChanged(float newValue)
+	{
+		int frameCount = frames.Length;
+		frameIdx = frameCount - (int)((newValue / slider.maxValue) * frameCount);
+		image.sprite = frames[frameIdx];
+	}
+}
