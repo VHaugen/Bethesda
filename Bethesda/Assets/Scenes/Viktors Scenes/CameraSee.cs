3fb68cd5b6815eb91a7aa277d6710b0d0a29c21f
Haugen <haugen@workstation-linux.lan>
2019-04-23 23:03:28 +0200

    Game Awards

--- Bethesda/Assets/Scenes/Viktors Scenes/CameraSee.cs
+++ Bethesda/Assets/Scenes/Viktors Scenes/CameraSee.cs
@@ -5,5 +5,21 @@ using UnityEngine;
 public class CameraSee : MonoBehaviour
 {
 
-    
+    public Color transColor;
+    private Color initColor;
+
+    void start()
+    {   
+        initColor = renderer.material.color;
+    }
+    public void SetTransparent()
+    {
+
+    }
+public void SetNormal()
+    {
+        
+    }
+
+
 }
