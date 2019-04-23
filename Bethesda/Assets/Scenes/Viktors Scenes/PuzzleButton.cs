96c07fe604229a03def7cced5f03b3e5a542142a
Viktor <vicke00@live.com>
2019-04-23 15:55:50 +0200

    slipad Main

--- Bethesda/Assets/Scenes/Viktors Scenes/PuzzleButton.cs
+++ Bethesda/Assets/Scenes/Viktors Scenes/PuzzleButton.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleButton : MonoBehaviour, IAttackable {
+public class PuzzleButton : MonoBehaviour, IAttackable
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     public virtual void TakeDamage(DamageParams args)
     {
         print("i Was Hit amarifht");
 
         Destroy(transform.GetComponent<PuzzleButton>());
         transform.parent.GetComponent<PuzzelButtonHandler>().TriggerWasHit();
-		
+        Destroy(transform.GetComponent<CapsuleCollider>());
+
+        transform.GetComponent<Rigidbody>().useGravity = true;
+
+
+
     }
+    
 }
